6853d41ab6a8bccc712459ec3dabb2d05af53e3c
santiago hurtado <sahurtaro@gmail.com>
2022-04-19 21:25:56 -0500

    useEffect en GifGrid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    useEffect( () => {
+        getGifs();
+    }, [] )
 
     const getGifs = async() => {
 
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=PdL52NXNmOfmPw5382NZ36YyRFiVAUDX'
         const resp = await fetch ( url );
-        const data = await resp.json();
+        const {data} = await resp.json();
         const gifs = data.map( img => {
             return {
                 id: img.id,
@@ -20,7 +24,7 @@ export const GifGrid = ({category}) => {
 
     }
 
-    getGifs();
+    // getGifs();
 
   return (
     <div>
